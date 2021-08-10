131890593f1e96d8a9bd2026c87e7c8636c0aa4a
RobertoMilani <46413967+RobertoMilani@users.noreply.github.com>
2021-08-10 15:29:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ async function postEntry(feed, entry) {
 	const result = await topics.post({
 		uid: posterUid,
 		title: entry.title,
-		content: 'Prova ' && entry.description && entry.link && entry.link.href,
+		content: (entry.description && '') + '<br/>From: ' + (entry.link && entry.link.href),
 		cid: feed.category,
 		tags: tags,
 	});
